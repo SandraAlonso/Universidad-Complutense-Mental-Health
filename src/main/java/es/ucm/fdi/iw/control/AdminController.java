f8f115830c24bd76695f6091908311d63aeaa264
sandraalonso <sandraalopa@gmail.com>
2020-06-10 19:48:51 +0200

    comit de nav y admin

--- src/main/java/es/ucm/fdi/iw/control/AdminController.java
+++ src/main/java/es/ucm/fdi/iw/control/AdminController.java
@@ -1,9 +1,13 @@
 package es.ucm.fdi.iw.control;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.persistence.EntityManager;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,60 +15,75 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import es.ucm.fdi.iw.LocalData;
+import es.ucm.fdi.iw.model.GroupAppointment;
 import es.ucm.fdi.iw.model.User;
 
 /**
  * Admin-only controller
+ * 
  * @author mfreire
  */
 @Controller()
 @RequestMapping("admin")
 public class AdminController {
-	
+
 	private static final Logger log = LogManager.getLogger(AdminController.class);
-	
-	@Autowired 
+
+	@Autowired
 	private EntityManager entityManager;
-	
+
 	@Autowired
 	private LocalData localData;
-	
+
 	@Autowired
 	private Environment env;
-	
+
 	@GetMapping("/")
 	public String index(Model model) {
 		model.addAttribute("activeProfiles", env.getActiveProfiles());
 		model.addAttribute("basePath", env.getProperty("es.ucm.fdi.base-path"));
 
-		model.addAttribute("users", entityManager.createQuery(
-				"SELECT u FROM User u").getResultList());
-		
+		model.addAttribute("users", entityManager.createQuery("SELECT u FROM User u").getResultList());
+
 		return "admin";
 	}
-	
+
 	@PostMapping("/toggleuser")
 	@Transactional
-	public String delUser(Model model,	@RequestParam long id) {
+	public String delUser(Model model, @RequestParam long id) {
 		User target = entityManager.find(User.class, id);
 		if (target.getEnabled() == 1) {
 			// disable
-			File f = localData.getFile("user", ""+id);
+			File f = localData.getFile("user", "" + id);
 			if (f.exists()) {
 				f.delete();
 			}
 			// disable user
-			target.setEnabled((byte)0); 
+			target.setEnabled((byte) 0);
 		} else {
 			// enable user
-			target.setEnabled((byte)1);
+			target.setEnabled((byte) 1);
 		}
 		return index(model);
-	}	
+	}
+
+	@PostMapping("/createUser")
+	@Transactional
+	public String createUser(Model model, HttpServletResponse response, @ModelAttribute @Valid User user,
+			BindingResult result, HttpSession session) {
+		User requester = (User) session.getAttribute("u");
+		User stored = entityManager.find(User.class, requester.getId());
+		entityManager.persist(user);
+		entityManager.flush();
+		return "redirect:/admin";
+	}
+
 }
