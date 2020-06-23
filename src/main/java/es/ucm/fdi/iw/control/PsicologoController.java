e4c84bdfd396fdb1d3090407b64c312245051c2e
sandraalonso <sandraalopa@gmail.com>
2020-06-23 17:07:08 +0200

    add user a cita grupal choluchionado

--- src/main/java/es/ucm/fdi/iw/control/PsicologoController.java
+++ src/main/java/es/ucm/fdi/iw/control/PsicologoController.java
@@ -28,7 +28,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 import es.ucm.fdi.iw.model.Appointment;
 import es.ucm.fdi.iw.model.EntradasPsicologo;
 import es.ucm.fdi.iw.model.GroupAppointment;
@@ -50,7 +49,6 @@ public class PsicologoController {
 	@Autowired
 	EntityManager entityManager;
 
-	
 	// Requester es el usuario que solicita la accion.
 	// Edited son los datos que obtenemos en la interfaz y por lo tanto, lo que
 	// queremos cambiar
@@ -101,25 +99,26 @@ public class PsicologoController {
 		int horaActual = groupAppointment.getStart_hour().compareTo(ahora);
 
 		if (fecha == 0 && horaActual > 0 && hora > 0 || fecha > 0 && hora > 0) {
-			
-			
-			TypedQuery<Appointment> query = entityManager.createNamedQuery("Appointment.allAppointmentsOfSameDate", Appointment.class); 
-			List<Appointment> lm = query.setParameter("username", stored.getId()).setParameter("date", groupAppointment.getDate()).setParameter("sth", groupAppointment.getStart_hour()).setParameter("fnh", groupAppointment.getFinish_hour()).getResultList();
-			
-			
-			if(lm.size()==0) {
-			groupAppointment.setCreator(stored);
-			entityManager.persist(groupAppointment);
-			entityManager.flush();
-			log.info("El usuario {} ha creado una cita grupal el dia {} a las {}.", stored.getFirstName(),
-					groupAppointment.getDate(), groupAppointment.getStart_hour());
-			return "redirect:/psicologo/horario";
-			}
-			else {
+
+			TypedQuery<Appointment> query = entityManager.createNamedQuery("Appointment.allAppointmentsOfSameDate",
+					Appointment.class);
+			List<Appointment> lm = query.setParameter("username", stored.getId())
+					.setParameter("date", groupAppointment.getDate())
+					.setParameter("sth", groupAppointment.getStart_hour())
+					.setParameter("fnh", groupAppointment.getFinish_hour()).getResultList();
+
+			if (lm.size() == 0) {
+				groupAppointment.setCreator(stored);
+				entityManager.persist(groupAppointment);
+				entityManager.flush();
+				log.info("El usuario {} ha creado una cita grupal el dia {} a las {}.", stored.getFirstName(),
+						groupAppointment.getDate(), groupAppointment.getStart_hour());
+				return "redirect:/psicologo/horario";
+			} else {
 				Problema p = new Problema("El usuario " + stored.getUsername() + " no puede añadir cita.");
 				model.addAttribute("problema", p);
-				log.info(
-						"El usuario {} no ha podido crear una cita grupal porque ya hay una cita a esa hora.",stored.getFirstName());
+				log.info("El usuario {} no ha podido crear una cita grupal porque ya hay una cita a esa hora.",
+						stored.getFirstName());
 				return horarioPsicologo(session, model, null);
 
 			}
@@ -132,8 +131,8 @@ public class PsicologoController {
 			return horarioPsicologo(session, model, null);
 
 		}
-}
-	
+	}
+
 	@RequestMapping("/deleteAppointment")
 	@Transactional
 	public String deleteAppointment(Model model, HttpServletResponse response, HttpSession session,
@@ -147,7 +146,8 @@ public class PsicologoController {
 			entityManager.remove(ga);
 			return "redirect:/psicologo/horario";
 		} else {
-			Problema p = new Problema("El usuario " + stored.getUsername() + " no puede eliminar una cita inexistente.");
+			Problema p = new Problema(
+					"El usuario " + stored.getUsername() + " no puede eliminar una cita inexistente.");
 			model.addAttribute("problema", p);
 			log.info("El usuario {} no puede eliminar una cita inexistente.", stored.getUsername());
 			return horarioPsicologo(session, model, null);
@@ -170,13 +170,15 @@ public class PsicologoController {
 			int horaActual = groupAppointment.getStart_hour().compareTo(ahora);
 
 			if (fecha == 0 && horaActual > 0 && hora > 0 || fecha > 0 && hora > 0) {
-				
-				
-				TypedQuery<Appointment> query = entityManager.createNamedQuery("Appointment.allAppointmentsOfSameDate", Appointment.class); 
-				List<Appointment> lm = query.setParameter("username", stored.getId()).setParameter("date", groupAppointment.getDate()).setParameter("sth", groupAppointment.getStart_hour()).setParameter("fnh", groupAppointment.getFinish_hour()).getResultList();
-				
-				
-				if(lm.size()==1) {
+
+				TypedQuery<Appointment> query = entityManager.createNamedQuery("Appointment.allAppointmentsOfSameDate",
+						Appointment.class);
+				List<Appointment> lm = query.setParameter("username", stored.getId())
+						.setParameter("date", groupAppointment.getDate())
+						.setParameter("sth", groupAppointment.getStart_hour())
+						.setParameter("fnh", groupAppointment.getFinish_hour()).getResultList();
+
+				if (lm.size() == 1) {
 					ga.setName(groupAppointment.getName());
 					ga.setDate(groupAppointment.getDate());
 					ga.setStart_hour(groupAppointment.getStart_hour());
@@ -186,22 +188,21 @@ public class PsicologoController {
 							stored.getFirstName(), groupAppointment.getName(), groupAppointment.getStart_hour(),
 							groupAppointment.getDate());
 					return "redirect:/psicologo/horario";
-				}
-				else {
+				} else {
 					Problema p = new Problema("El usuario " + stored.getUsername() + " no puede modificar esta cita.");
 					model.addAttribute("problema", p);
 					log.info("El usuario {} no puede modificar una cita inexistente.", stored.getUsername());
 					return horarioPsicologo(session, model, null);
 				}
-			}
-			else {
+			} else {
 				Problema p = new Problema("El usuario " + stored.getUsername() + " no puede modificar esta cita.");
 				model.addAttribute("problema", p);
 				log.info("El usuario {} no puede modificar una cita inexistente.", stored.getUsername());
 				return horarioPsicologo(session, model, null);
 			}
 		} else {
-			Problema p = new Problema("El usuario " + stored.getUsername() + " no puede modificar una cita inexistente.");
+			Problema p = new Problema(
+					"El usuario " + stored.getUsername() + " no puede modificar una cita inexistente.");
 			model.addAttribute("problema", p);
 			log.info("El usuario {} no puede modificar una cita inexistente.", stored.getUsername());
 			return horarioPsicologo(session, model, null);
@@ -220,20 +221,20 @@ public class PsicologoController {
 		List<User> lu = new ArrayList<User>();
 		for (int i = 0; i < values.length; ++i) {
 			List<User> us = query.setParameter("username", values[i]).getResultList();
-			if(!us.isEmpty()) {
-			User u = us.get(0);
-			if (u != null) {
-				if(u.hasRole(Role.PSICOLOGO) || u.hasRole(Role.ADMIN)) break;
-				lu.add(u);
-			} else // ya se han añadido todos los pacientes
-				break;
+			if (!us.isEmpty()) {
+				User u = us.get(0);
+				if (u != null) {
+					if (!u.hasRole(Role.PSICOLOGO) && !u.hasRole(Role.ADMIN))
+						lu.add(u);
+				} else // ya se han añadido todos los pacientes
+					break;
 			}
 		}
 
 		if (lu != null) {
 			ga.setPatient(lu);
-			log.info("El usuario {} ha añadido a la cita grupal {} a {} usuarios.", stored.getFirstName(),
-					ga.getName(), lu.size());
+			log.info("El usuario {} ha añadido a la cita grupal {} a {} usuarios.", stored.getFirstName(), ga.getName(),
+					lu.size());
 		}
 		return "redirect:/psicologo/horario";
 	}
@@ -249,15 +250,13 @@ public class PsicologoController {
 		if (u != null) {
 			if (user.getTreatment() != "") {
 				u.setTreatment(user.getTreatment());
-				log.info(
-						"El usuario {} ha recetado {} al paciente {}.",
-						stored.getFirstName(), user.getTreatment(), user.getFirstName());
+				log.info("El usuario {} ha recetado {} al paciente {}.", stored.getFirstName(), user.getTreatment(),
+						user.getFirstName());
 			}
 			if (user.getDisorder() != "") {
 				u.setDisorder(user.getDisorder());
-				log.info(
-						"El usuario {} ha diagnosticado {} al paciente {}.",
-						stored.getFirstName(), user.getDisorder(), user.getFirstName());
+				log.info("El usuario {} ha diagnosticado {} al paciente {}.", stored.getFirstName(), user.getDisorder(),
+						user.getFirstName());
 			}
 		} else {
 			log.info(
@@ -280,20 +279,17 @@ public class PsicologoController {
 		if (id != null) {
 			User pat = entityManager.find(User.class, id);
 			LocalDate date = LocalDate.now();
-			if(!description.getDescription().equals("")) {
-			description.setPatient(pat);
-			description.setDate(date);
-			pat.addPsychologistEntry(description);
+			if (!description.getDescription().equals("")) {
+				description.setPatient(pat);
+				description.setDate(date);
+				pat.addPsychologistEntry(description);
 
-			entityManager.persist(description);
-			entityManager.flush();
-			log.info("El usuario {} ha añadido una nueva entrada al paciente {}, ya tiene {} entradas.",
-					stored.getFirstName(), pat.getFirstName(), pat.getDescription().size());
-			}
-			else {
-				log.info(
-						"El usuario {} no puede escribir una entrada vacia.",
-						stored.getFirstName());
+				entityManager.persist(description);
+				entityManager.flush();
+				log.info("El usuario {} ha añadido una nueva entrada al paciente {}, ya tiene {} entradas.",
+						stored.getFirstName(), pat.getFirstName(), pat.getDescription().size());
+			} else {
+				log.info("El usuario {} no puede escribir una entrada vacia.", stored.getFirstName());
 			}
 		} else {
 			log.info(
@@ -302,32 +298,31 @@ public class PsicologoController {
 		}
 		return "redirect:/psicologo/pacientes";
 	}
-	
-	
+
 	@GetMapping(path = "/getAppointments/{id}", produces = "application/json")
 	@ResponseBody
 	public Appointment getAppointments(HttpSession session, @PathVariable long id) {
 
 		User requester = (User) session.getAttribute("u");
 		User stored = entityManager.find(User.class, requester.getId());
-		Appointment ga= entityManager.find(Appointment.class, id);
-		boolean esSuya=false;
-		for(int i =0;i<stored.getCreatorAppointments().size();i++) {
-			if(stored.getCreatorAppointments().get(i).getID()==id) {
-				esSuya=true;
+		Appointment ga = entityManager.find(Appointment.class, id);
+		boolean esSuya = false;
+		for (int i = 0; i < stored.getCreatorAppointments().size(); i++) {
+			if (stored.getCreatorAppointments().get(i).getID() == id) {
+				esSuya = true;
 				break;
 			}
-			
+
 		}
-		if(!esSuya) {
-			for(int i =0;i<stored.getGroupAppointmentsPatient().size();i++) {
-			if(stored.getGroupAppointmentsPatient().get(i).getID()==id) {
-				esSuya=true;
-				break;
-			}
+		if (!esSuya) {
+			for (int i = 0; i < stored.getGroupAppointmentsPatient().size(); i++) {
+				if (stored.getGroupAppointmentsPatient().get(i).getID() == id) {
+					esSuya = true;
+					break;
+				}
 			}
 		}
-		if(esSuya)
+		if (esSuya)
 			return ga;
 		else
 			return null;
